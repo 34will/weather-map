6f22be2856ec22e655666247c6c470bfcad78d91
34will <wgilmour@hotmail.co.uk>
2018-11-20 21:03:35 +0000

    Added Display selection to the context menu.

--- main.js
+++ main.js
@@ -11,78 +11,150 @@ const argumentRegex = /--(.*?)=(.*)/;
 
 let mainWindow = null;
 let tray = null;
+let x = null;
+let y = null;
+let width = null;
+let height = null;
+let screen = null;
 
 function GetNumericArgument(arguments, name, def) {
-	def = def || 0;
-	
-	const arg = arguments.find(function (argument) { return argument[0] == name; });
-	let res = def;
-	if (arg)
-		res = parseInt(arg[1]);
-	if (isNaN(res))
-		res = def;
-	return res;
+    def = def || 0;
+
+    const arg = arguments.find(function (argument) {
+        return argument[0] == name;
+    });
+    let res = def;
+    if (arg)
+        res = parseInt(arg[1]);
+    if (isNaN(res))
+        res = def;
+    return res;
 }
 
-function GetBounds(ElectronScreen, x, y, width, height, screen) {
-	let displays = ElectronScreen.getAllDisplays();
-	console.log(x, y, width, height, screen, displays);
-	if (displays.length <= screen)
-		displays = displays.slice(0, screen);
-	
-	let lastDisplay = displays.slice(-1)[0];
-	let ret = { x: lastDisplay.bounds.x + x, y: y, width: width, height: height };
-	console.log(lastDisplay, ret);
-	return ret;
+function GetBounds(ElectronScreen) {
+    let displays = ElectronScreen.getAllDisplays();
+    if (displays.length > screen)
+        displays = displays.slice(0, screen);
+
+    let lastDisplay = displays.slice(-1)[0];
+    let ret = {
+        x: lastDisplay.bounds.x + x,
+        y: lastDisplay.bounds.y + y,
+        width: width,
+        height: height
+    };
+    return ret;
 }
 
-function CreateWindow () {
-	const ElectronScreen = electron.screen;
-	
-	const arguments = process
-		.argv
-		.filter(function (argument) { return argumentRegex.test(argument); })
-		.map(function (argument) {
-			return argument
-				.match(argumentRegex)
-				.splice(1);
-		});
-		
-	const x = GetNumericArgument(arguments, "x");
-	const y = GetNumericArgument(arguments, "y");
-	const width = GetNumericArgument(arguments, "width", 438);
-	const height = GetNumericArgument(arguments, "height", 444);
-	const screen = GetNumericArgument(arguments, "screen", 1);
-	
-	let bounds = GetBounds(ElectronScreen, x, y, width, height, screen);
-	mainWindow = new BrowserWindow({x: bounds.x, y: bounds.y, width: bounds.width, height: bounds.height, frame: false, resizeable: false, skipTaskbar: true, minimizable: false});
+function Reposition(ElectronScreen) {
+    mainWindow.setBounds(GetBounds(ElectronScreen));
+}
 
-	mainWindow.loadURL("https://earth.nullschool.net/#current/wind/surface/level/patterson=-2.89,54.14,1705");
+function CalculateDisplays(ElectronScreen) {
+    let displays = ElectronScreen.getAllDisplays();
+    let displayContextMenuItems = [];
+    for (let i = 0; i < displays.length; i++) {
+        displayContextMenuItems.push({
+            type: "radio",
+            label: "Display #" + (i + 1) + " (" + displays[i].bounds.width + "x" + displays[i].bounds.height + ")",
+            checked: (i + 1) == screen,
+            click() {
+                screen = i + 1;
+                displayContextMenuItems.forEach(function (x) {
+                    x.checked = false;
+                });
+                displayContextMenuItems[i].checked = true;
+                Reposition(ElectronScreen);
+            }
+        });
+    }
 
-	mainWindow.on("closed", function () { mainWindow = null; });
-	
-	mainWindow.setMenu(null);
-	mainWindow.setIgnoreMouseEvents(true);
+    const contextMenu = Menu.buildFromTemplate(displayContextMenuItems.concat([{
+            type: "separator"
+        }, {
+            label: "Reposition",
+            click() {
+                Reposition(ElectronScreen);
+            }
+        },
+        {
+            label: "Exit",
+            click() {
+                App.quit();
+            }
+        }
+    ]));
+    tray.setContextMenu(contextMenu);
+}
 
-	const iconPath = path.join(__dirname, 'Weather Map.ico');
-	const icon = NativeImage.createFromPath(iconPath);
-	tray = new Tray(icon);
-	const contextMenu = Menu.buildFromTemplate([
-		{label: "Reposition", click() { mainWindow.setBounds(GetBounds(ElectronScreen, x, y, width, height, screen)); }},
-		{label: "Exit", click() { App.quit(); }}
-	]);
-	tray.setToolTip("Weather Map");
-	tray.setContextMenu(contextMenu);
+function CreateWindow() {
+    const ElectronScreen = electron.screen;
+
+    const arguments = process
+        .argv
+        .filter(function (argument) {
+            return argumentRegex.test(argument);
+        })
+        .map(function (argument) {
+            return argument
+                .match(argumentRegex)
+                .splice(1);
+        });
+
+    ElectronScreen.on("display-added", function () {
+        CalculateDisplays(ElectronScreen);
+    });
+    ElectronScreen.on("display-removed", function () {
+        CalculateDisplays(ElectronScreen);
+    });
+    ElectronScreen.on("display-metrics-changed", function () {
+        CalculateDisplays(ElectronScreen);
+    });
+
+    x = GetNumericArgument(arguments, "x");
+    y = GetNumericArgument(arguments, "y");
+    width = GetNumericArgument(arguments, "width", 438);
+    height = GetNumericArgument(arguments, "height", 444);
+    screen = GetNumericArgument(arguments, "screen", 1);
+
+    let bounds = GetBounds(ElectronScreen);
+    mainWindow = new BrowserWindow({
+        x: bounds.x,
+        y: bounds.y,
+        width: bounds.width,
+        height: bounds.height,
+        frame: false,
+        resizeable: false,
+        skipTaskbar: true,
+        minimizable: false
+    });
+
+    mainWindow.loadURL("https://earth.nullschool.net/#current/wind/surface/level/patterson=-2.89,54.14,1705");
+
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.setIgnoreMouseEvents(true);
+
+    const iconPath = path.join(__dirname, 'Weather Map.ico');
+    const icon = NativeImage.createFromPath(iconPath);
+
+    tray = new Tray(icon);
+    tray.setToolTip("Weather Map");
+
+    CalculateDisplays(ElectronScreen);
 }
 
 App.on("ready", CreateWindow);
 
 App.on("window-all-closed", function () {
-  if (process.platform !== "darwin")
-    App.quit();
+    if (process.platform !== "darwin")
+        App.quit();
 });
 
 App.on("activate", function () {
-  if (mainWindow === null)
-    CreateWindow();
+    if (mainWindow === null)
+        CreateWindow();
 });
